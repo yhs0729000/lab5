63563838afa0eef8a8c8fc459ef28122e984507f
yhs0729000 <yhs072900000@gmail.com>
2017-05-22 23:00:17 +0800

    0

---
+++ lab5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+#include "HugeInt.h"
+
+int main()
+{HugeInt x;
+HugeInt y(28825252);
+HugeInt z("314159265358979323846");
+HugeInt result;
+cin >> x;
+result = x+y;
+cout << x << "+" << y << " = " << result << endl;
+result = z - x;
+cout << result << endl;
+return 0;
+}
